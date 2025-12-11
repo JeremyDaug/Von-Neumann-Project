bfbee5895ed5180a18981960222e0387e17c1ff1
Jeremy <Jeremy.Daug@gmail.com>
2025-12-10 20:13:59 -0600

    Barebones camera move and zoom functionality

--- src/screens/game_screen.rs
+++ src/screens/game_screen.rs
@@ -1,6 +1,6 @@
 use std::f32::consts::PI;
 
-use bevy::{prelude::*, app::{App, Update}, asset::Assets, ecs::{schedule::IntoScheduleConfigs, system::{Commands, Res, ResMut}}, input::{ButtonInput, keyboard::KeyCode}, log::info, mesh::Mesh, platform::collections::HashMap, sprite_render::{ColorMaterial, Wireframe2dPlugin}, state::{condition::in_state, state::NextState}};
+use bevy::{app::{App, Update}, asset::Assets, camera::CameraProjection, ecs::{schedule::IntoScheduleConfigs, system::{Commands, Res, ResMut}}, input::{ButtonInput, keyboard::KeyCode, mouse::MouseWheel}, log::info, mesh::Mesh, platform::collections::HashMap, prelude::*, sprite_render::{ColorMaterial, Wireframe2dPlugin}, state::{condition::in_state, state::NextState}};
 
 use crate::{game::{body::Body, orbital::Orbital}, game_state::{self, GameState}};
 
@@ -29,6 +29,10 @@ pub fn game_plugin(app: &mut App) {
     .insert_resource(ClearColor(Color::NONE))
     .init_resource::<GameData>()
     .add_systems( OnEnter(GameState::Game), load_game)
+    .add_systems(Update,
+        move_camera_2d.run_if(in_state(GameState::Game)))
+    .add_systems(Update, 
+        animation_tick.run_if(in_state(GameState::Game)))
     //.add_systems(OnExit(GameState::Game), clear_game)
     .add_systems(Update, 
         (keypress_actions).run_if(in_state(GameState::Game))
@@ -42,12 +46,39 @@ pub fn game_plugin(app: &mut App) {
 
 // TODO: Add a clear fame function which clears out entities upon returning to main menu, but not on pause.
 
+fn move_camera_2d(
+    time: Res<Time>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut mouse_wheel: MessageReader<MouseWheel>,
+    mut cam: Query<(&mut Transform, &mut Projection), With<Camera2d>>,
+) {
+    if let Ok((mut tf, mut proj)) = cam.single_mut() {
+        if let Projection::Orthographic(proj) = &mut *proj {
+            // pan with WASD
+            let mut move_dir = Vec2::ZERO;
+            let pan_speed = 400.0;
+            if keyboard.pressed(KeyCode::KeyA) { move_dir.x -= 1.0; }
+            if keyboard.pressed(KeyCode::KeyD) { move_dir.x += 1.0; }
+            if keyboard.pressed(KeyCode::KeyW) { move_dir.y += 1.0; }
+            if keyboard.pressed(KeyCode::KeyS) { move_dir.y -= 1.0; }
+            tf.translation += (move_dir.normalize_or_zero() * pan_speed * proj.scale * time.delta_secs()).extend(0.0);
+
+            // zoom function
+            for ev in mouse_wheel.read() {
+                let delta = if ev.y > 0.0 { 0.9 } else { 1.0 / 0.9 };
+                proj.scale = (proj.scale * delta).clamp(0.1, 10.0);
+            }
+        } else {
+            info!("Projection was not Orthographic?!");
+        }
+    }
+}
+
 fn load_game(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut game_data: ResMut<GameData>,
-    time: Res<Time>,
 ) {
     // if game has already been loaded, skip this function.
     if game_data.game_loaded {
@@ -68,19 +99,31 @@ fn load_game(
         OrbitalId(0),
     ));
 
-    let x = f32::sin(time.elapsed_secs() * PI) * 100.0;
-    let y = f32::cos(time.elapsed_secs() * PI) * 100.0;
-
     commands.spawn((
         Mesh2d(testhandle2),
         MeshMaterial2d(materials.add(Color::srgba(1.0, 0.0, 0.0, 1.0))),
-        Transform::from_xyz(x, y, 0.0),
+        Transform::from_xyz(0.0, 100.0, 0.0),
         OrbitalId(1),
     ));
 }
 
-fn animation_tick() {
-
+fn animation_tick(
+    mut _orb_data: ResMut<GameData>,
+    mut query: Query<(&mut Transform, &OrbitalId)>,
+    time: Res<Time>,
+) {
+    // deal with game speed checking here.
+    // if time since last tick is not enough, skip the tick.
+    // With time having passed successfully,
+    for (mut transform, OrbitalId(id)) in query.iter_mut() {
+        // for now, this is a super simple calculation. only move the second object.
+        if *id == 1 {
+            let x = f32::sin(time.elapsed_secs() * PI) * 100.0;
+            let y = f32::cos(time.elapsed_secs() * PI) * 100.0;
+            transform.translation.x = x;
+            transform.translation.y = y;
+        }
+    }
 }
 
 pub fn keypress_actions(
